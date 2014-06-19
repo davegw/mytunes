1ef6a5baf635541a7d2c0da34156ab19b93542a0
Hack Reactor Students <dgertmenianwong@gmail.com>
2014-06-18 20:24:49 -0700

    Pass all tests: - Add enqueue(), dequeue() - Enable play next song on current song end - Built queue template and view - Auto-update queue view on model change

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,34 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
+    this.on('add', function(){
+      if (this.length === 1) {
+        this.playFirst();
+      }
+    }, this);
+
+    this.on('dequeue', function(){
+      this.dequeue();
+    }, this);
+
+    this.on('ended', function(){
+      this.dequeue();
+      if (this.length > 0) {
+        this.playFirst();
+      }
+    }, this);
+  },
+
+  playFirst: function() {
+    this.at(0).play();
+  },
+
+  enqueue: function(song) {
+    this.add(song);
+  },
+
+  dequeue: function() {
+    this.remove(this.at(0));
   }
 
 });
