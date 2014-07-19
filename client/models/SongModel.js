d22196eadaef32aead5901c95b85a5e3fde36530
davidgertmenianwong <dgertmenianwong@gmail.com>
2014-07-19 11:21:58 -0700

    Store play count and rating in local storage

--- client/models/SongModel.js
+++ client/models/SongModel.js
@@ -2,8 +2,12 @@
 var SongModel = Backbone.Model.extend({
 
   initialize: function(){
-    this.set('counter', 0);
-    this.set('rating', 0);
+    // Use local storage to persist song count and rating.
+    var storedCount = window.localStorage[this.get('title') + 'Counter'] || 0;
+    var storedRating = window.localStorage[this.get('title') + 'Rating'] || 0;
+    
+    this.set('counter', Number(storedCount));
+    this.set('rating', Number(storedRating));
   },
 
   play: function(){
@@ -14,14 +18,17 @@ var SongModel = Backbone.Model.extend({
 
   increment: function () {
     this.set('counter', this.get('counter')+1);
+    window.localStorage.setItem(this.get('title') + 'Counter', this.get('counter'));
   },
 
   upvote: function () {
     this.set('rating', this.get('rating')+1);
+    window.localStorage.setItem(this.get('title') + 'Rating', this.get('rating'));
   },
 
   downvote: function () {
     this.set('rating', this.get('rating')-1);
+    window.localStorage.setItem(this.get('title') + 'Rating', this.get('rating'));
   },
 
   enqueue: function(){
